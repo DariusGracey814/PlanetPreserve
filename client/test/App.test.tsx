4f529d2d05e832c2bd73c8951599be96c6acc57c
DariusGracey <Dariusgracey814@gmail.com>
2023-03-03 18:38:50 -0500

    created main App file unit test

---
+++ client/test/App.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../src/App";
+
+test("renders planet preserve h1", () => {
+  // 1. Render component
+  render(<App />);
+
+  // 2. Get element that should exists in component from the screen
+  const h1Element = screen.getByText(/Planet Preserve/);
+
+  // 3. Expect element to be in the document if exists test is passed
+  expect(h1Element).toBeInTheDocument();
+});
