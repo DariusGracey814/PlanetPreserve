e76fe05975b90b121e7f20a0e2b35bc52b066666
DariusGracey <Dariusgracey814@gmail.com>
2023-03-03 19:28:30 -0500

    add main tag to main App to hold children sections

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,9 @@ import Container from "./Components/UI/Container/Container";
 const App: React.FC = () => {
   return (
     <div>
-      <Container>
-        <h1 className="text-3xl">Planet Preserve</h1>
-      </Container>
+      <main>
+        <Container></Container>
+      </main>
     </div>
   );
 };
