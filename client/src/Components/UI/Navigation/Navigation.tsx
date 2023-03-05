1c205b53f6710193d8e4af43c18ed68fbacbcd6a
DariusGracey <Dariusgracey814@gmail.com>
2023-03-04 22:03:11 -0500

    create naviagtion login signup toogle page logic

--- client/src/Components/UI/Navigation/Navigation.tsx
+++ client/src/Components/UI/Navigation/Navigation.tsx
@@ -1,11 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { formDataActions } from "../../../../store/forms";
+import { RootState } from "../../../../store/store";
 
 const Navigation: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const enteredUsername = useSelector(
+    (state: RootState) => state.FormData.username
+  );
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.FormData.isAuthenticated
+  );
+
+  const loginState = useSelector(
+    (state: RootState) => state.FormData.loginState
+  );
+
+  const togglePage = (): void => {
+    dispatch(formDataActions.setLoginState());
+    navigate(`${!loginState ? "signup" : "login"}`);
+  };
+
   return (
     <nav className="flex justify-end">
       <Link className="btn nav-link text-white" to="/">
-        Sign up
+        <button onClick={togglePage}> Sign up</button>
       </Link>
     </nav>
   );
