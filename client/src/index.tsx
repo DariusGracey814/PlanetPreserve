31ff8ef24091ea5f6d4d2a875ce25d1bad6b3ca6
DariusGracey <Dariusgracey814@gmail.com>
2023-03-04 20:58:55 -0500

    connect redux store to application

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import App from "./App";
 import "./index.css";
 
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
