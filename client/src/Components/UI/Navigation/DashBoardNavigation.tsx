45ae4cdcef3a7d5ca449247dc29cb24513b61d90
DariusGracey <Dariusgracey814@gmail.com>
2023-03-10 15:46:28 -0500

    create dashboard navigation component

---
+++ client/src/Components/UI/Navigation/DashBoardNavigation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DashBoardNavigation: React.FC = () => {
+  return (
+    <aside className="border4 border-red-600">
+      {/* Dashboard Navigation */}
+      <nav>
+        <li></li>
+      </nav>
+    </aside>
+  );
+};
+
+export default DashBoardNavigation;
