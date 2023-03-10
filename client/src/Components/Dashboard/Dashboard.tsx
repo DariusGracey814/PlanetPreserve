3b5010f87f18e618a126a42374e169074156c87d
DariusGracey <Dariusgracey814@gmail.com>
2023-03-10 16:25:05 -0500

    add dashboard navigation top username information

--- client/src/Components/Dashboard/Dashboard.tsx
+++ client/src/Components/Dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import DashBoardNavigation from "../UI/Navigation/DashBoardNavigation";
 
 const Dashboard: React.FC = () => {
   return (
-    <section>
+    <section className="grid dashboard_grid w-screen h-screen">
       <DashBoardNavigation />
       <div className="border-2 border-blue-600">Dashboard</div>
     </section>
--- client/src/Components/UI/Navigation/DashBoardNavigation.tsx
+++ client/src/Components/UI/Navigation/DashBoardNavigation.tsx
@@ -5,17 +5,21 @@ const DashBoardNavigation: React.FC = () => {
   return (
     <aside className="border4 border-red-600 dashboard_nav--bg">
       {/* Dashboard Navigation */}
-      <nav>
+      <nav className="px-6 py-5">
         {/* User icon and name */}
-        <div>
-          <div className="flex items-center justify-center w-12 h-12 rounded-full shadow-md bg-white">
-            <FaUserAlt className="dashboard-icon" />
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full shadow-md bg-white">
+              <FaUserAlt className="dashboard-icon" />
+            </div>
+            <p className="text-white username ml-3">Darius Gracey</p>
           </div>
-          <p className="text-white">Darius Gracey</p>
           <div>
             <FaInfoCircle className="text-white text-1xl" />
           </div>
         </div>
+
+        {/* Navigation Links */}
         <li></li>
       </nav>
     </aside>
