c4743c8fa8f3c51f7e19b6642f38d052e7efdfea
DariusGracey <Dariusgracey814@gmail.com>
2023-03-10 17:50:42 -0500

    build dashboard navigation and style component

--- client/src/Components/UI/Navigation/DashBoardNavigation.tsx
+++ client/src/Components/UI/Navigation/DashBoardNavigation.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { FaUserAlt, FaInfoCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {
+  FaUserAlt,
+  FaInfoCircle,
+  FaQrcode,
+  FaEdit,
+  FaTrashAlt,
+} from "react-icons/fa";
+import { AiOutlineSearch, AiFillQuestionCircle } from "react-icons/ai";
+import { BiMessage } from "react-icons/bi";
+import { IoIosAddCircle } from "react-icons/io";
 
 const DashBoardNavigation: React.FC = () => {
   return (
-    <aside className="border4 border-red-600 dashboard_nav--bg">
+    <aside className="dashboard_nav--bg">
       {/* Dashboard Navigation */}
-      <nav className="px-6 py-5">
+      <nav className="px-8 py-5">
         {/* User icon and name */}
         <div className="flex justify-between items-center">
           <div className="flex items-center">
@@ -19,8 +29,84 @@ const DashBoardNavigation: React.FC = () => {
           </div>
         </div>
 
+        {/* Search Bar */}
+        <div>
+          <form className="relative search">
+            <input
+              className="rounded-lg"
+              type="text"
+              placeholder="Quick Find"
+            />
+            <button className="absolute search-btn" type="submit">
+              <AiOutlineSearch className="search_icon" />
+            </button>
+          </form>
+        </div>
+
+        {/* General Navigation Link */}
+        <div className="general-text mb-4">
+          <p className="general text-white">GENERAL</p>
+        </div>
+
         {/* Navigation Links */}
-        <li></li>
+        <ul className="mb-14">
+          <li className="flex items-center mb-4">
+            <FaQrcode className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              Overview
+            </Link>
+          </li>
+
+          <li className="flex items-center mb-4">
+            <FaUserAlt className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              My Posts
+            </Link>
+          </li>
+
+          <li className="flex items-center mb-4">
+            <IoIosAddCircle className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              Add Posts
+            </Link>
+          </li>
+
+          <li className="flex items-center mb-4">
+            <FaEdit className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              Edit Post
+            </Link>
+          </li>
+
+          <li className="flex items-center mb-4">
+            <FaTrashAlt className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              Delete Post
+            </Link>
+          </li>
+        </ul>
+
+        {/* Support Navigation Link */}
+        <div className="general-text mb-4">
+          <p className="general text-white">SUPPORT</p>
+        </div>
+
+        {/* Support Navigation */}
+        <ul>
+          <li className="flex items-center mb-4">
+            <AiFillQuestionCircle className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              Help
+            </Link>
+          </li>
+
+          <li className="flex items-center mb-4">
+            <BiMessage className="nav-icon" />
+            <Link to="/dashboard" className="nav-text ml-4">
+              Feedback
+            </Link>
+          </li>
+        </ul>
       </nav>
     </aside>
   );
