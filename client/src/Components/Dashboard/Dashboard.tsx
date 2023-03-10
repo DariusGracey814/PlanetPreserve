0ccf73635e767427c5dd3ca8defebf7993181702
DariusGracey <Dariusgracey814@gmail.com>
2023-03-10 16:08:28 -0500

    build and add dashboard navigation styles

--- client/src/Components/Dashboard/Dashboard.tsx
+++ client/src/Components/Dashboard/Dashboard.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import DashBoardNavigation from "../UI/Navigation/DashBoardNavigation";
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <DashBoardNavigation />
+      <div className="border-2 border-blue-600">Dashboard</div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- client/src/Components/UI/Navigation/DashBoardNavigation.tsx
+++ client/src/Components/UI/Navigation/DashBoardNavigation.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { FaUserAlt, FaInfoCircle } from "react-icons/fa";
 
 const DashBoardNavigation: React.FC = () => {
   return (
-    <aside className="border4 border-red-600">
+    <aside className="border4 border-red-600 dashboard_nav--bg">
       {/* Dashboard Navigation */}
       <nav>
+        {/* User icon and name */}
+        <div>
+          <div className="flex items-center justify-center w-12 h-12 rounded-full shadow-md bg-white">
+            <FaUserAlt className="dashboard-icon" />
+          </div>
+          <p className="text-white">Darius Gracey</p>
+          <div>
+            <FaInfoCircle className="text-white text-1xl" />
+          </div>
+        </div>
         <li></li>
       </nav>
     </aside>
