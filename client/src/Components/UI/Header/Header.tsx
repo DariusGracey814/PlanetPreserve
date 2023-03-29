3c548fd84940b843c90c77bdac21241a4f8aaf5c
DariusGracey <Dariusgracey814@gmail.com>
2023-03-28 22:12:39 -0400

    update main header display based on logged in user

--- client/src/Components/UI/Header/Header.tsx
+++ client/src/Components/UI/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../store/store";
 import Navigation from "../Navigation/Navigation";
 
 const Header: React.FC = () => {
+  const authenticated = useSelector(
+    (state: RootState) => state.AuthSlice.authenticatedUser
+  );
+
   return (
     <header className="header w-full absolute top-0 left-0 justify-end py-5 px-5">
-      <Navigation />
+      {/* Display main nav if user is not authenticated */}
+      {!authenticated ? <Navigation /> : null}
     </header>
   );
 };
